7aa7673bde5b0e16359ed1311f6cbb17754105b2
ethandb23 <119861532+ethandb23@users.noreply.github.com>
2023-02-02 01:03:27 -1000

    More styling, can see favs now

--- project2/src/JokesList.js
+++ project2/src/JokesList.js
@@ -3,7 +3,8 @@ import styles from './JokesList.module.css';
 
 const JokesList = () => {
   const [jokes, setJokes] = useState([]);
-  const [currentJokeIndex, setCurrentJokeIndex] = useState(0);
+  const [currentJoke, setCurrentJoke] = useState({});
+  const [favorites, setFavorites] = useState([]);
 
   useEffect(() => {
     fetchJoke();
@@ -12,38 +13,49 @@ const JokesList = () => {
   const fetchJoke = () => {
     fetch('https://icanhazdadjoke.com/', { headers: { 'Accept': 'application/json' } })
       .then(response => response.json())
-      .then(data => {
-        setJokes([...jokes, data.joke]);
-      });
+      .then(data => setCurrentJoke({ id: data.id, joke: data.joke, completed: false }));
   };
 
-  const handleAddJoke = () => {
+  const addToFavorites = () => {
+    setFavorites([...favorites, currentJoke]);
+    setJokes([...jokes, currentJoke]);
     fetchJoke();
   };
 
-  const handleDeleteJoke = () => {
-    setJokes(jokes.filter((joke, index) => index !== currentJokeIndex));
-    setCurrentJokeIndex(currentJokeIndex - 1);
-  };
-
-  const handleNextJoke = () => {
-    setCurrentJokeIndex(currentJokeIndex + 1);
+  const removeFromFavorites = jokeId => {
+    setFavorites(favorites.filter(joke => joke.id !== jokeId));
   };
 
   return (
     <div className={styles.container}>
-      <h2>{jokes[currentJokeIndex]}</h2>
-      <div className={styles.buttonContainer}>
-        <button className={styles.button} onClick={handleAddJoke}>
-          Add to Favorite List
+      <div className={styles.currentJoke}>
+        {currentJoke.joke}
+        <button onClick={addToFavorites} className={styles.favoriteButton}>
+          Add to Favorites
         </button>
-        <button className={styles.button} onClick={handleDeleteJoke}>
-          Delete Joke
-        </button>
-        <button className={styles.button} onClick={handleNextJoke}>
+        <button onClick={fetchJoke} className={styles.nextButton}>
           Next Joke
         </button>
       </div>
+      <FavoritesList favorites={favorites} removeFromFavorites={removeFromFavorites} />
+    </div>
+  );
+};
+
+const FavoritesList = ({ favorites, removeFromFavorites }) => {
+  return (
+    <div className={styles.favoritesContainer}>
+      <h2>Favorites</h2>
+      <ul>
+        {favorites.map(joke => (
+          <li key={joke.id}>
+            {joke.joke}
+            <button onClick={() => removeFromFavorites(joke.id)} className={styles.removeButton}>
+              Remove
+            </button>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 };
